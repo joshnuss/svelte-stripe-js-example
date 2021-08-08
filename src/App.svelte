7f17c93aff87ef40ad5138f9f5b5fc066e502108
Josh Nussbaum <joshnuss@gmail.com>
2021-08-08 01:45:51 -0400

    Basic example

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,26 @@
 <script>
+  import { Container, CardNumber, CardExpiry, CardCvc } from './components'
+  const stripe = Stripe('pk_test_EFUTAJ2pGPBKqTiQPTiGIGsD');
+
+  let container
+
+  async function submit() {
+    const card = container.getElement()
+    const result = await stripe.createToken(card)
+
+    alert(`Got: ${result.token.card.id}`)
+  }
 </script>
 
-<main>
-</main>
+<Container {stripe} bind:this={container}>
+  <form on:submit|preventDefault={submit}>
+    <CardNumber/>
+    <CardExpiry/>
+    <CardCvc/>
+
+    <button>Pay</button>
+  </form>
+</Container>
 
 <style>
 </style>
